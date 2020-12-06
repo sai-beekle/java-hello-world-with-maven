0f131e451570a8706765362e86d3908166b83b96
Sai <58899893+sai-beekle@users.noreply.github.com>
2020-12-06 22:12:25 +0530

    second commit

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package hello;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void mai(String[] args) {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("The current local time is: " + currentTime);
 
